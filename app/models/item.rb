8c62ac4ac97dc8b07bdc96de576e2cc5b7f94bce
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-04 22:29:40 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :day
+
 
   with_options presence: true do 
     validates :name
     validates :description
-    validates :category_id
-    validates :status_id
-    validates :cost_id
-    validates :area_id
-    validates :days_id
     validates :item_price
     validates :user
   end
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :cost_id, numericality: { other_than: 1 } 
+  validates :area_id, numericality: { other_than: 0 } 
+  validates :days_id, numericality: { other_than: 1 } 
 end
