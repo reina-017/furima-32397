834396852ca29539217483532a1b290f0982b367
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-05 14:13:56 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path     
+   else
+     render :new      
+   end
+  end
+
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,18 +6,19 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :cost
   belongs_to :area
-  belongs_to :day
+  belongs_to :days
 
 
   with_options presence: true do 
     validates :name
     validates :description
-    validates :item_price
     validates :user
+    validates :image
   end
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :cost_id, numericality: { other_than: 1 } 
-  validates :area_id, numericality: { other_than: 0 } 
-  validates :days_id, numericality: { other_than: 1 } 
+
+    validates :item_price, numericality: { only_integer: true, greater_than:300, less_than:9999999}
+    validates :category_id, numericality: { only_integer: true, other_than: 1} 
+    validates :cost_id,     numericality: { only_integer: true, other_than: 1} 
+    validates :area_id,     numericality: { only_integer: true, other_than: 0} 
+    validates :days_id,     numericality: { only_integer: true, other_than: 1}  
 end
