d56f227ca156620b553e2b1ffd54ea57cbb2f24c
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-05 18:29:59 +0900

    itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,12 +13,13 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :user
-    validates :image
+    # validates :image
   end
 
-    validates :item_price, numericality: { only_integer: true, greater_than:300, less_than:9999999}
+    validates :item_price,  numericality: { only_integer: true, greater_than:300, less_than:9999999}
     validates :category_id, numericality: { only_integer: true, other_than: 1} 
     validates :cost_id,     numericality: { only_integer: true, other_than: 1} 
+    validates :status_id,   numericality: { only_integer: true, other_than: 1} 
     validates :area_id,     numericality: { only_integer: true, other_than: 0} 
     validates :days_id,     numericality: { only_integer: true, other_than: 1}  
 end
