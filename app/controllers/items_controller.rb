5b62dd0d5df185bb3ce08bc658f92c3e85fae2a9
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-07 15:17:01 +0900

    修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(item_params) && current_user.id = @item.user_id
+    if @item.update(item_params) && current_user.id =＝ @item.user_id
       redirect_to item_path
     else
       render :edit
