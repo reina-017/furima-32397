6903c1fe7f35afecd249eb5be078208db57c39b9
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-09 23:48:02 +0900

    ボタン微調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id
+    if @item.order.present? || current_user.id == @item.user_id
       redirect_to root_path
     end  
     @order_form = OrderForm.new
