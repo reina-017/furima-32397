7c2cbd9cfe74d2f034c2947601d8bed42fae119a
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-09 12:35:48 +0900

    order_formモデル単体テスト

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -9,13 +9,9 @@ class OrderForm
     validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
     validates :phone_number, format: { with: /\A\d{11}\z/, message: 'を入力してください' }
     validates :order
-
-   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :city
     validates :block
-   end
   end
-    validates :building, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
 
     def save
       # 購入記録の情報を保存
