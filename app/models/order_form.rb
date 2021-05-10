7b6ff55f73ad37fdc0133b6590f8ed0eb866fd84
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-10 15:31:07 +0900

    微調整

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,7 +8,6 @@ class OrderForm
     validates :prefecture_id, numericality: { only_integer: true, other_than: 1 }
     validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: 'を入力してください' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'を入力してください' }
-    validates :order
     validates :city
     validates :block
     validates :token
