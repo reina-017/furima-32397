e8ece1e100fe96bced2893a8473325f54d473c4a
reina-017 <79751222+reina-017@users.noreply.github.com>
2021-05-10 15:28:36 +0900

    Revert "Merge pull request #9 from reina-017/revert-8-商品購入機能の実装"
    This reverts commit 8ae9e615bf723d244aac260cc27f3d4a7b529ff3, reversing
    changes made to 56385e655686359a641e7119c3f24852df988b14.

---
+++ app/models/order_form.rb
@@ -0,0 +1,22 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :prefecture_id, numericality: { only_integer: true, other_than: 1 }
+    validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: 'を入力してください' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'を入力してください' }
+    validates :order
+    validates :city
+    validates :block
+    validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building,
+                       phone_number: phone_number, order_id: order.id)
+  end
+end
